db2a421bfa512f5264bed204a780f20f3ff6be43
ortemx <printerq@yandex.ru>
2023-10-07 21:36:34 +0300

    lab 3 done

--- PSR4Checker.ps1
+++ PSR4Checker.ps1
@@ -1,3 +1,8 @@
 phpcbf.bat --standard=PSR12 Task02/guess-number/bin/guess-number.php
 phpcbf.bat --standard=PSR12 Task02/guess-number/src/Controller.php
 phpcbf.bat --standard=PSR12 Task02/guess-number/src/View.php
+
+phpcbf.bat --standard=PSR12 Task03/guess-number/bin/guess-number.php
+phpcbf.bat --standard=PSR12 Task03/guess-number/src/Controller.php
+phpcbf.bat --standard=PSR12 Task03/guess-number/src/Model.php
+phpcbf.bat --standard=PSR12 Task03/guess-number/src/View.php
---
+++ Task03/guess-number/bin/guess-number.php
@@ -0,0 +1,9 @@
+#!/usr/bin/env php
+
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use function ortemx\GuessNumber\Controller\startGame;
+
+startGame();
