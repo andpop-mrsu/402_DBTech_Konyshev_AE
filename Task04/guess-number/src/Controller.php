fc8d982eaa5ade9a399631ab74c538512936dd4d
ortemx <printerq@yandex.ru>
2023-10-15 16:04:33 +0300

    Добавленна база данных

--- Task04/guess-number/src/Controller.php
+++ Task04/guess-number/src/Controller.php
@@ -3,21 +3,13 @@
 namespace ortemx\GuessNumber\Controller;
 
 use ortemx\GuessNumber\GameModel\GameModel;
-use function ortemx\GuessNumber\View\showRules;
-use function ortemx\GuessNumber\View\showWelcomeMessage;
-use function ortemx\GuessNumber\View\showDefeatMessage;
-use function ortemx\GuessNumber\View\showExitMessage;
-use function ortemx\GuessNumber\View\showWinningMessage;
-use function ortemx\GuessNumber\View\showComparisonMessage;
-use function ortemx\GuessNumber\View\showAdminPanel;
-use function ortemx\GuessNumber\View\askPlayerName;
-use function ortemx\GuessNumber\View\showMenu;
-use function ortemx\GuessNumber\View\showAllGames;
-use function ortemx\GuessNumber\View\showTopPlayers;
-use function ortemx\GuessNumber\View\showReplayOfGame;
-use function cli\input;
+use ortemx\GuessNumber\GameView\GameView;
+use ortemx\GuessNumber\Logs\Logs;
 
-function gameLoop($model)
+use function cli\input;
+use function cli\prompt;
+
+function gameLoop($model, $view)
 {
     $settings = $model->getSettings();
     $attempt_count = $settings['attemptCount'];
@@ -26,114 +18,102 @@ function gameLoop($model)
     $attempt_number = 0;
     $guested_number = $model->guessNumber();
 
-    askPlayerName();
-    $player_name = input();
+    $player_name = prompt("Enter your name");
 
-    $game_date = date("Y-m-d H:i:s");
-    $outcome = null;
+    $logs = new Logs();
+    $logs->player_name = $player_name;
+    $logs->max_number = $maxNumber;
+    $logs->secret_number = $guested_number;
 
-    // для данных по конуретной игре
-    $attemps = [];
-    $entered_numbers = [];
-    $answers = [];
-    showRules($player_name, $settings);
-    while ($attempt_number <= $attempt_count) {
+    $view->showRules($player_name, $settings);
+    while (++$attempt_number <= $attempt_count) {
         $entered_number = input();
-        $entered_numbers[] = $entered_number;
-        $attempt_number++;
-        $attemps[] = $attempt_number;
+        $logs->entered_numbers[] = $entered_number;
+        $logs->attempts[] = $attempt_number;
+
+        if (filter_var($entered_number, FILTER_VALIDATE_INT) === false) {
+            $view->notANumberError();
+            $logs->answers[] = "NaN";
+            if ($attempt_number == $attempt_count) {
+                $view->defeatMessage($guested_number);
+                $logs->outcome = "lose";
+            }
+            continue;
+        }
+        if ($entered_number > $maxNumber) {
+            $view->outOfRangeError();
+            $logs->answers[] = "OoRange";
+            if ($attempt_number == $attempt_count) {
+                $view->defeatMessage($guested_number);
+                $logs->outcome = "lose";
+            }
+            continue;
+        }
         if ($entered_number == $guested_number) {
-            showWinningMessage($attempt_number);
-            $outcome = "won";
-            $answers[] = "Guess";
+            $view->winningMessage($attempt_number);
+            $logs->outcome = "won";
+            $logs->answers[] = "Guess";
             break;
         } elseif ($entered_number == 0) {
-            showExitMessage();
-            $outcome = "exit";
-            $answers[] = "Exit";
+            $view->exitMessage();
+            $logs->outcome = "exit";
+            $logs->answers[] = "Exit";
             break;
         } elseif ($guested_number < $entered_number) {
-            showComparisonMessage("less", $attempt_number, $attempt_count);
-            $answers[] = "less";
+            $view->ComparisonMessage("less", $attempt_number, $attempt_count);
+            $logs->answers[] = "less";
         } else {
-            showComparisonMessage("greater", $attempt_number, $attempt_count);
-            $answers[] = "greater";
+            $view->ComparisonMessage("greater", $attempt_number, $attempt_count);
+            $logs->answers[] = "greater";
         }
         if ($attempt_number == $attempt_count) {
-            showDefeatMessage($guested_number);
-            $outcome = "lose";
-            $answers[] = "Defeat";
+            $view->defeatMessage($guested_number);
+            $logs->outcome = "lose";
+            $logs->answers[] = "Defeat";
             break;
         }
     }
-    $info = [
-        'dateGame' => $game_date,
-        'playerName' => $player_name,
-        'maxNumber' => $maxNumber,
-        'secretNumber' => $guested_number,
-        'gameResult' => $outcome,
-        'attempts' => $attemps,
-        'enteredNumbers' => $entered_numbers,
-        'answers' => $answers,
-    ];
-    $model->saveGameIntoDatabase($info);
+
+    $model->saveGameIntoDatabase($logs);
 }
 
-function administrating()
-{
-    print("\033[2J\033[;H");
-    showAdminPanel();
-    while (true) {
-        $password = input();
-        if ($password == "1234") {
-            // Пересоздание базы данных
-            // Удаление данных об игре
-            // Удаление данных об игроке
-            // Изменение настроек игры
-        } elseif ($password == "exit") {
-            return;
-        }
-    }
-}
 function startGame()
 {
     $model = new GameModel("gamedb.db");
+    $view = new GameView();
     while (true) {
-        showWelcomeMessage();
-        showMenu();
+        $view->welcomeMessage();
+        $view->menu();
         $choise = input();
         switch ($choise) {
             case 1:
-                gameLoop($model);
+                gameLoop($model, $view);
                 break;
             case 2:
                 $games = $model->getGames();
-                showAllGames($games);
+                count($games) ? $view->showGames($games) : print("There are no saved games");
                 break;
             case 3:
                 $games = $model->getGames("WON");
-                showAllGames($games);
+                count($games) ? $view->showGames($games, "WON") : print("There are no games");
                 break;
             case 4:
                 $games = $model->getGames("LOSE");
-                showAllGames($games);
+                count($games) ? $view->showGames($games, "LOSE") : print("There are no games");
                 break;
             case 5:
                 $players = $model->getTopPlayers();
-                showTopPlayers($players);
+                count($players) ? $view->showTopPlayers($players) : print("There are no players");
                 break;
             case 6:
-                $gameid = input();
+                $gameid = prompt("Enter game id");
                 $moves = $model->getReplayOfGame($gameid);
-                showReplayOfGame($gameid, $moves);
-                break;
-            case "admin":
-                administrating();
+                $view->showReplayOfGame($gameid, $moves);
                 break;
             case 0:
                 exit();
             default:
-                echo ("Invalid choise");
+                print("Invalid choise");
         }
         input();
         print("\033[2J\033[;H");
--- Task04/guess-number/src/GameModel.php
+++ Task04/guess-number/src/GameModel.php
@@ -11,12 +11,17 @@ class GameModel
     private $attempt_count;
     public function __construct($db_path)
     {
-        $this->db_path = 'gamedb.db';
+        $this->db_path = $db_path;
         $this->createTables();
         $this->setSettings(10, 10);
         $settings = $this->getSettings();
         $this->max_number = $settings['maxNumber'];
         $this->attempt_count = $settings['attemptCount'];
+    }
+
+    public function getDbPath()
+    {
+        return $this->db_path;
     }
 
     private function createTables()
@@ -31,12 +36,12 @@ class GameModel
                 secretNumber INTEGER,
                 gameResult TEXT
             )";
-    
+
             $settings_table = "CREATE TABLE Settings(
                 maxNumber INTEGER,
                 attemptCount INTEGER
             )";
-    
+
             $replays_table = "CREATE TABLE Replays(
                 gameId INTEGER,
                 attempt INTEGER,
@@ -73,9 +78,9 @@ class GameModel
         $sql = "";
         if ($gameFilter == "ALL") {
             $sql = "SELECT * FROM GamesInfo";
-        } else if ($gameFilter == "WON") {
+        } elseif ($gameFilter == "WON") {
             $sql = "SELECT * FROM GamesInfo WHERE gameResult = 'won'";
-        } else if ($gameFilter == "LOSE") {
+        } elseif ($gameFilter == "LOSE") {
             $sql = "SELECT * FROM GamesInfo WHERE gameResult = 'lose'";
         }
         $result = $db->query($sql);
@@ -90,14 +95,21 @@ class GameModel
     public function getTopPlayers()
     {
         $db = new SQLite3($this->db_path);
-        $sql = "SELECT playerName, COUNT(CASE WHEN gameResult = 'won' THEN 1 END) AS wins, COUNT(CASE WHEN gameResult = 'lose' THEN 1 END) AS losses FROM GamesInfo GROUP BY playerName ORDER BY wins DESC";
+        $sql =
+        "SELECT 
+            playerName, 
+            COUNT(CASE WHEN gameResult = 'won' THEN 1 END) AS wins, 
+            COUNT(CASE WHEN gameResult = 'lose' THEN 1 END) AS losses 
+        FROM GamesInfo 
+        GROUP BY playerName 
+        ORDER BY wins DESC";
         $result = $db->query($sql);
         $players = [];
         while ($player = $result->fetchArray(SQLITE3_ASSOC)) {
             $players[] = $player;
         }
         $db->close();
-        return $players;        
+        return $players;
     }
 
     public function getReplayOfGame($gameId)
@@ -113,33 +125,33 @@ class GameModel
         return $moves;
     }
 
-    public function saveGameIntoDatabase($info)
+    public function saveGameIntoDatabase($logs)
     {
         $dbPath = 'gamedb.db';
         $db = new SQLite3($dbPath);
         $sql = "INSERT INTO GamesInfo 
         (dateGame, playerName, maxNumber, secretNumber, gameResult) VALUES(
-            '" . $info['dateGame'] . "',
-            '" . $info['playerName'] . "',
-            '" . $info['maxNumber'] . "',
-            '" . $info['secretNumber'] . "',
-            '" . $info['gameResult'] . "'
+            '" . $logs->date_game . "',
+            '" . $logs->player_name . "',
+            '" . $logs->max_number . "',
+            '" . $logs->secret_number . "',
+            '" . $logs->outcome . "'
         )";
         $db->exec($sql);
         $lastId = $db->lastInsertRowID();
-        for ($i = 0; $i < count($info['attempts']); $i++) {
+        for ($i = 0; $i < count($logs->attempts); $i++) {
             $sql = "INSERT INTO Replays
             (gameId, attempt, enteredNumber, replay) VALUES(
                 '" . $lastId . "',
-                '" . $info['attempts'][$i] . "',
-                '" . $info['enteredNumbers'][$i] . "',
-                '" . $info['answers'][$i] . "'
+                '" . $logs->attempts[$i] . "',
+                '" . $logs->entered_numbers[$i] . "',
+                '" . $logs->answers[$i] . "'
             )";
             $db->exec($sql);
         }
         $db->close();
     }
-    
+
     public function guessNumber()
     {
         return rand(1, $this->max_number);
